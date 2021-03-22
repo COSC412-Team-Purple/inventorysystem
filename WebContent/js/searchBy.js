e0d0e964af48781a9f66f6154ffb9707fd38edd1
JeremiahFailla <jpf405@gmail.com>
2021-03-22 13:45:02 -0400

    changed display items section to a table and added some checkboxs for filtering the search

---
+++ WebContent/js/searchBy.js
@@ -0,0 +1,48 @@
+const searchByNameCheckbox = document.getElementById('searchByName');
+const searchByCategoryCheckbox = document.getElementById('searchByCategory');
+const searchByMngDeptCheckbox = document.getElementById(
+  'searchByManagingDepartment'
+);
+const searchByPriceRangeCheckbox = document.getElementById(
+  'searchByPriceRange'
+);
+
+const searchByNameInput = document.getElementById('itemName');
+const searchByCategoryInput = document.getElementById('itemCategory');
+const searchByMngDeptInput = document.getElementById('itemManagingDepartment');
+const searchByPriceRangeMinInput = document.getElementById('priceMin');
+const searchByPriceRangeMaxInput = document.getElementById('priceMax');
+
+searchByNameCheckbox.addEventListener('change', (e) => {
+  if (e.target.checked) {
+    searchByNameInput.disabled = false;
+    return;
+  }
+  searchByNameInput.disabled = true;
+});
+
+searchByCategoryCheckbox.addEventListener('change', (e) => {
+  if (e.target.checked) {
+    searchByCategoryInput.disabled = false;
+    return;
+  }
+  searchByCategoryInput.disabled = true;
+});
+
+searchByMngDeptCheckbox.addEventListener('change', (e) => {
+  if (e.target.checked) {
+    searchByMngDeptInput.disabled = false;
+    return;
+  }
+  searchByMngDeptInput.disabled = true;
+});
+
+searchByPriceRangeCheckbox.addEventListener('change', (e) => {
+  if (e.target.checked) {
+    searchByPriceRangeMinInput.disabled = false;
+    searchByPriceRangeMaxInput.disabled = false;
+    return;
+  }
+  searchByPriceRangeMinInput.disabled = true;
+  searchByPriceRangeMaxInput.disabled = true;
+});
