2e5948fc6f0d088883de94534f1e1c48b38b33a1
orysia29 <opopow1@students.towson.edu>
2021-04-02 20:24:17 -0400

    Updated ResetPassword and ItemSearch servlets

--- src/com/java/inventorysystem/AuthenticateMember/ResetPassword.java
+++ src/com/java/inventorysystem/AuthenticateMember/ResetPassword.java
@@ -2,15 +2,20 @@ package com.java.inventorysystem.AuthenticateMember;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
 import com.java.inventorysystem.Utilities.*;
 /**
  * Servlet implementation class ResetPassword
@@ -45,33 +50,101 @@ public class ResetPassword extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		conn = DBConnectionUtility.getDatabaseConnection();
-		System.out.println("ResetPassword servlet connecting to DB");
-		try {
-			Statement stmt = conn.createStatement();
-			String username = request.getParameter("?"); //need a name for the reset username field
-			String password = request.getParameter("?"); //need a name for the reset password field
-			String query = "UPDATE "; //TODO: SQL for replacing the password for a particular user 
-			ResultSet rs = stmt.executeQuery(query);
+		String username = request.getParameter("username"); //need a name for the reset username field
+		String newPassword = request.getParameter("password"); //need a name for the reset password field
+		JSONObject returnJson = new JSONObject();
+		
+		if(verifyPassword(newPassword))
+		{
+			try {
+				conn = DBConnectionUtility.getDatabaseConnection();
+				
+				if(setNewPassword(username, newPassword))
+				{		
+					returnJson.put("success", true);
+					ClientResponseUtility.writeToClient(response, returnJson);
+				}
+				else
+				{
+					returnJson.put("success", false);
+					ClientResponseUtility.writeToClient(response, returnJson);
+				}
+				
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 			
-			conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
+			
 		}
+		else
+		{
+			//Send JSON back saying that the operation was not successful
+			returnJson.put("success", false);
+			ClientResponseUtility.writeToClient(response, returnJson);
+		}
+		
+		
 	}
 	
 	//Check to make sure the new password meets all the password criteria
 	private boolean verifyPassword(String password){
-		boolean success = false;
+		boolean validchars = true; //no special characters such as space, tab or the 'ESC' key
+		boolean number = false;    //at least one number
+		boolean lowercase = false; //at least one upper case character
+		boolean uppercase = false; //at least one lower case character
+		boolean symbol = false;    //at least one symbol like ! or # or &
+		boolean length = false;    //at least MIN_PASS_LENGTH characters long
+		final int MIN_PASS_LENGTH = 6;
 		
-		return success;
+		if(password.length() >= MIN_PASS_LENGTH)
+		{
+			length = true;
+		
+			for(int i = 0; i < password.length(); i++)
+			{
+				char c = password.charAt(i);
+				if(c < '!' || c > '~')
+				{
+					validchars = false;
+					break;
+				}
+				
+				if(c >= '0' && c <= '9')
+				{
+					number = true;
+				}
+				
+				if(c >= 'a' && c <= 'z')
+				{
+					lowercase = true;
+				}
+				
+				if(c >= 'A' && c <= 'Z')
+				{
+					uppercase = true;
+				}
+				
+				if((c >= '!' && c <= '/') || (c >= ':' && c <= '@') || (c >= '[' && c <= '`') || (c >= '{' && c <= '~'))
+				{
+					symbol = true;
+				}
+			}
+		}
+
+		return validchars && number && lowercase && uppercase && symbol && length;
 	}
 	
 	//Builds SQL string containing the member's new password hash that will be saved to the DB
-	private boolean setNewPassword(int memberId, String password) throws SQLException {
-		boolean success = false;
+	private boolean setNewPassword(String username, String password) throws SQLException {
 		
-		return success;
+		String query = ""; //TODO: make SQL statement to set the new password
+		PreparedStatement stmt = conn.prepareStatement(query);
+		ResultSet rs = stmt.executeQuery();
+
+		//Resultset should always contain something if it succeeds I think. If resultset is empty then it failed
+		//This means the new password can't be set for some reason such as the username doesn't exist 
+		return rs.next();
 	}
 
 }
--- src/com/java/inventorysystem/ItemSearch/ItemSearch.java
+++ src/com/java/inventorysystem/ItemSearch/ItemSearch.java
@@ -7,12 +7,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
 import com.java.inventorysystem.Utilities.*;
 /**
  * Servlet implementation class ItemSearch
@@ -40,19 +44,53 @@ public class ItemSearch extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		conn = DBConnectionUtility.getDatabaseConnection();
-		System.out.println("ItemSearch servlet connecting to DB");
+		String item_name = request.getParameter("item_name");
+		String category  = request.getParameter("item_category");
+		String dept      = request.getParameter("item_dept");
+		float priceMin   = Float.parseFloat(request.getParameter("item_price_max"));
+		float priceMax   = Float.parseFloat(request.getParameter("item_price_min"));
+		JSONObject returnJson = new JSONObject();
+		
 		try {
-			Statement stmt = conn.createStatement();
-			String item = request.getParameter("search-item");
-			String category = request.getParameter("item-category");
-			String department = request.getParameter("managing-department");
-			//Need a name attribute for the min and max price range fields
-			String query = "SELECT * FROM items where name = " + item; //when user searches by item name; need category, department, price, etc.
-			ResultSet rs = stmt.executeQuery(query);
+			conn = DBConnectionUtility.getDatabaseConnection();
 			
-			while(rs.next()) {
-				response.getWriter().write(rs.getString(1) + "\n");
+			ResultSet item_info = searchItem(item_name, category, dept, priceMin, priceMax);
+			
+			int hits = 0;
+			
+			while(item_info.next()) {
+				hits++;
+			
+				int itemID          = item_info.getInt("item_id");
+				String itemName     = item_info.getString("item_name");
+				float itemPrice     = item_info.getFloat("price");
+				int itemQuantity    = item_info.getInt("item_quat");
+				String itemModel    = item_info.getString("item_model");
+				String itemLocation = item_info.getString("item_loc");
+				String itemDept     = item_info.getString("dept_name");
+				String itemCategory = item_info.getString("category");
+				Date itemDate       = item_info.getDate("purchase_date");
+				String itemBrand    = item_info.getString("item_brand");
+				String itemText     = item_info.getString("item_memo");
+				
+				returnJson.put("item_id", itemID);
+				returnJson.put("item_name", itemName);
+				returnJson.put("price", itemPrice);
+				returnJson.put("item_quat", itemQuantity);
+				returnJson.put("item_model", itemModel);
+				returnJson.put("item_loc", itemLocation);
+				returnJson.put("dept_name", itemDept);
+				returnJson.put("category", itemCategory);
+				returnJson.put("purchase_date", itemDate);
+				returnJson.put("item_brand", itemBrand);
+				returnJson.put("item_memo", itemText);
+								
+				ClientResponseUtility.writeToClient(response, returnJson);
+				response.setStatus(200);
+			}
+			
+			if(hits == 0) {
+				response.sendError(400, "item not in DB");
 			}
 
 			conn.close();
@@ -69,9 +107,29 @@ public class ItemSearch extends HttpServlet {
 	}
 	
 	//Build SQL statement for searching for an item in the DB
-	private ResultSet searchItem(String name, String category, String dept, int priceMin, int priceMax) throws SQLException {
+	private ResultSet searchItem(String name, String category, String dept, float priceMin, float priceMax) throws SQLException {
 		
-		String query = "";
+		//item name is mandatory
+
+		//set defaults for category and dept in preparation for the sql statement
+		if(category.equals("any"))
+		{
+			category = "*";
+		}
+		
+		if(dept.equals("any"))
+		{
+			dept = "*";
+		}
+		
+		String query = "SELECT * FROM items WHERE category = " + category + " AND dept_name = " + dept;
+		
+		if( !(priceMin == 0.0 && priceMax == 0.0) && priceMin <= priceMax )
+		{
+			//If both are 0 then ignore price condition
+			query = query + " AND price >= " + priceMin + " AND price <= " + priceMax;
+		}
+		
 		PreparedStatement stmt = conn.prepareStatement(query);
 		ResultSet rs = stmt.executeQuery();
 			
